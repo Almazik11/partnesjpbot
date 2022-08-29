ab6f101ed93ddd1e9f9a3fd3a76f0dcc738774b8
Almaz123 <81878723+Almazik11@users.noreply.github.com>
2022-08-29 21:31:25 +0200

    Update init.js

--- src/commands/partner/init.js
+++ src/commands/partner/init.js
@@ -11,14 +11,14 @@
  */
 exports.run = async (client, message, args) => {
   if (args[0] === undefined) {
-    return client.embed.send(message, { desc: '> <a:gemoji_19:933092171744165898> ─ Пожалуйста, укажите канал.' })
+    return client.embed.send(message, { desc: '>  <:PandaHugg:966717408440090654> ─ Пожалуйста, укажите канал.' })
   }
   const channel = client.guilds.cache.get(message.guild.id).channels.cache.find(channel => [channel.name, channel.id].includes(args[0].replace(/[<#>]/g, '')))
   if (channel) {
     client.database.run('UPDATE settings SET partner = ? WHERE guildid = ?', [channel.id, message.guild.id])
-    client.embed.send(message, { desc: `> <a:gemoji_2:919528268380643328> ─ Успешно! Сейчас вы можете установить описание по команде \`${client.config.prefix}desc\` и сделать бамп по команде \`${client.config.prefix}bump\` !` })
+    client.embed.send(message, { desc: `>  <:yes:966272635329720320> ─ Успешно! Сейчас вы можете установить описание по команде \`${client.config.prefix}desc\` и сделать бамп по команде \`${client.config.prefix}bump\` !` })
   } else {
-    client.embed.send(message, { desc: '> <a:gemoji_1:919528250064117780> ─ Указан несуществующий канал.' })
+    client.embed.send(message, { desc: '>  <:no:966272561753227264> ─ Указан несуществующий канал.' })
   }
 }
 
