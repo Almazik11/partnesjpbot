05438be4904aa39be89c6f6a3911489d1804c7e1
Almaz123 <81878723+Almazik11@users.noreply.github.com>
2022-08-29 21:55:42 +0200

    Update desc.js

--- src/commands/partner/desc.js
+++ src/commands/partner/desc.js
@@ -12,20 +12,20 @@
 exports.run = async (client, message, args) => {
   const desc = args.join(' ')
   if (desc === undefined || desc === '') {
-    return client.embed.send(message, { desc: '> <a:gemoji_1:919528250064117780> ─ Укажите описание гильдии. Обратите внимание, что ваше приглашение в гильдию будет прикреплено автоматически.' })
+    return client.embed.send(message, { desc: '> <:heart:967000646446383144> ─ Укажите описание гильдии. Обратите внимание, что ваше приглашение в гильдию будет прикреплено автоматически.' })
   }
 
   if (desc.length > client.config.ad.desc.max_length) {
-    return client.embed.send(message, { desc: `> <a:gemoji_1:919528250064117780> ─ Описание не может быть длиннее ${client.config.ad.desc.max_length} символов.` })
+    return client.embed.send(message, { desc: `>  <:no:966272561753227264> ─ Описание не может быть длиннее ${client.config.ad.desc.max_length} символов.` })
   }
   if (desc.length < client.config.ad.desc.min_length) {
-    return client.embed.send(message, { desc: `> <a:gemoji_1:919528250064117780> ─ Описание должно содержать не менее ${client.config.ad.desc.min_length} символов.` })
+    return client.embed.send(message, { desc: `>  <:no:966272561753227264> ─ Описание должно содержать не менее ${client.config.ad.desc.min_length} символов.` })
   }
   if (desc.includes('http') || desc.includes('@everyone') || desc.includes('@here')) {
-    return client.embed.send(message, { desc: '> <a:gemoji_1:919528250064117780> ─ Пожалуйста, никаких ссылок или упоминаний в описании.' })
+    return client.embed.send(message, { desc: '>  <:PandaHugg:966717408440090654> ─ Пожалуйста, никаких ссылок или упоминаний в описании.' })
   }
   client.database.run('UPDATE settings SET desc = ? WHERE guildid = ?', [desc, message.guild.id])
-  client.embed.send(message, { desc: '> <a:gemoji_2:919528268380643328> ─ Описание успешно обновлено.' })
+  client.embed.send(message, { desc: '>  <:yes:966272635329720320> ─ Описание успешно обновлено.' })
 }
 
 /** Command Config */
